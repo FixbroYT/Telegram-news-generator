51bb4fd323941647968c812553a54e3fc2c2c45a
FixbroYT <135733422+FixbroYT@users.noreply.github.com>
2025-03-13 22:10:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,20 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from dotenv import load_dotenv
 from ai_request import get_post_text
+import os
+import threading
+import http.server
+import socketserver
+
+
+def run_dummy_server():
+    port = int(os.environ.get("PORT", 8080))  # Render требует, чтобы сервис слушал порт
+    handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", port), handler) as httpd:
+        httpd.serve_forever()
+
+threading.Thread(target=run_dummy_server, daemon=True).start()
+
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
