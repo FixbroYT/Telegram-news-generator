4c1a8a77af3d44e8e1c98bad35bb89b8afc88cef
FixbroYT <135733422+FixbroYT@users.noreply.github.com>
2025-03-15 20:03:13 +0100

    Update server.py

--- server.py
+++ server.py
@@ -6,21 +6,12 @@ from bot import start_bot
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return "Bot is running!"
-  
-def run_bot():
-    while True:
-        try:
-            start_bot()
-        except Exception as e:
-            print(f"Ошибка: {e}, перезапуск через 5 секунд...")
-            time.sleep(5)
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
 
 
-threading.Thread(target=run_bot, daemon=True).start()
+threading.Thread(target=run_flask, daemon=True).start()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    asyncio.run(start_bot())
